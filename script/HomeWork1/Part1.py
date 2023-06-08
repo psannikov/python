880f72194301b3daa4e725ad350d1eac4cfd78bb
psannikov <p.sannikov@volga.rt.ru>
2023-06-08 22:33:43 +0400

    ДЗ1

--- script/HomeWork1/Part1.py
+++ script/HomeWork1/Part1.py
@@ -10,19 +10,12 @@ TABLE_NAME = 'okved'
 # Определяем переменные и коннекты
 df = pd.DataFrame
 conn = sqlite3.connect(DB_NAME)
-# Open the ZIP file
+# Работаем с файлом и записываем результат в базу
 with zipfile.ZipFile(ZIP_PATH, 'r') as z:
-    # Iterate over each file in the ZIP archive
     for file_name in z.namelist():
-        # Check if the file is a JSON file
         if file_name.endswith('.json'):
-            # Read the JSON file into a list of dictionaries
             with z.open(file_name) as f:
                 data = json.load(f)
-                # Create a pandas DataFrame from the JSON data
                 df = pd.DataFrame(data)
-                # Process the DataFrame as needed
-# Insert the DataFrame data into the SQLite database
 df.to_sql(TABLE_NAME, conn, if_exists='replace', index=False)
-# Close the database connection
 conn.close()
